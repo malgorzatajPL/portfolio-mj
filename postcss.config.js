5998aca29bab75b78d20ff348799a94ddd80727d
Malgorzata Jaskiewicz <malgorzatahannajaskiewicz@gmail.com>
2025-04-19 20:10:53 +0100

    remove index.css and add postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
-  
+import postcssImport from 'postcss-import';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+const config = {
+  plugins: [
+    postcssImport(),
+    tailwindcss(),
+    autoprefixer(),
+  ],
+};
+
+export default config;
